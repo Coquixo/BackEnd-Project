3ee18bf451afcc714964225810865059a0cac238
Alex Lopez <coquixo98@gmail.com>
2022-10-30 18:58:03 +0100

    created index, config, added modules and router, we can start working on new branches

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const db = require('./db/db')
+const router = require('./router')
+require('./models/associations')
+
+
+
+const PORT = 3000;
+
+//MiddleWares:
+
+app.use(express.json());
+app.use(router)
+
+
+app.listen(PORT, () => {
+
+    console.log(`Server oppened at port ${PORT}`);
+
+    db.authenticate()
+        .then(() => {
+            console.log('Connected to the database');
+        })
+        .catch(err => { console.log('Not connecter to the database') })
+});
---
+++ router.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+// const UserRoutes = require('./views/UserRoutes')
+// const ProfileRoutes = require('./views/ProfileRoutes')
+
+
+//middleware para las rutas de usuario
+// router.use('/user', UserRoutes)
+// router.use('/profile', ProfileRoutes)
+
+
+module.exports = router
